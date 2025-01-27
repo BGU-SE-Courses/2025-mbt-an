7db261d6679c591b1880fab6e89ed9ab3f1d74b8
aseelza <zatout@post.bgu.ac.il>
2025-01-27 09:29:44 +0200

    ..

--- Provengo/moodle/spec/js/behavior.js
+++ Provengo/moodle/spec/js/behavior.js
@@ -1,31 +1,14 @@
 
 
-//bthread('setupAdmin', function () {
-//    let adminSession = new SeleniumSession('adminSession', 'chrome'); // Initialize the admin session
-//    adminSession.start(BASE_URL); // Start the session at the base URL
-//
-//    adminSession.adminLogin_1();
-//    request(Event('admin_login_complete'));
-//});
-//
-//
-//bthread('setupStudent', function () {
-//    let studentSession = new SeleniumSession('studentSession', 'chrome'); // Initialize the admin session
-//    studentSession.start(BASE_URL); // Start the session at the base URL
-//    studentSession.studentLogin_1();
-//    request(Event('student_login_complete'));
-//});
-
-
 
 bthread('Student moving to the next page in a test', function () {
   //  waitFor(Event('student_login_complete'));
     let studentSession1 = new SeleniumSession('studentSession1', 'chrome'); // Initialize the admin session
     studentSession1.start(BASE_URL); // Start the session at the base URL
     studentSession1.studentLogin_1();
-    ensureWindowAvailable(studentSession1);
+  //  ensureWindowAvailable(studentSession1);
     studentSession1.studentGoToCourse();
-    ensureWindowAvailable(studentSession1);
+//    ensureWindowAvailable(studentSession1);
     studentSession1.studentGoToQuiz();
     request(Event('student_moving_complete'));
 });
@@ -36,9 +19,9 @@ bthread('Admin removing student from extra-time group', function(){
     let adminSession1 = new SeleniumSession('adminSession1', 'chrome'); // Initialize the admin session
     adminSession1.start(BASE_URL); // Start the session at the base URL
     adminSession1.adminLogin_1();
-    ensureWindowAvailable(adminSession1);
+ //   ensureWindowAvailable(adminSession1);
     adminSession1.studentGoToCourse();
-    ensureWindowAvailable(adminSession1);
+   // ensureWindowAvailable(adminSession1);
     adminSession1.adminGoToGroup();
     adminSession1.adminRemoveStudent();
     request(Event('admin_removing_complete'));
